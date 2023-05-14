acfeb59a8c5d7c852ff21490b08297bc87b3feca
gms1995 <132783179+gms1995@users.noreply.github.com>
2023-05-15 00:43:25 +0800

    Add files via upload

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM python:3.11-buster
+
+WORKDIR /code
+RUN apt-get update -y && apt-get install -y cron logrotate
+COPY requirements.txt .
+RUN pip install -r requirements.txt
+RUN pip install gunicorn
+RUN pip install supervisor
+COPY . .
+ENV FLASK_APP rcon.connection
+ENV PYTHONPATH /code/
+RUN chmod +x entrypoint.sh
+RUN chmod +x manage.py
+ENV LOGGING_FILENAME=startup.log
+
+ENTRYPOINT [ "/code/entrypoint.sh" ]
---
+++ entrypoint.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -e
+set -x 
+
+env
+if [ "$1" == 'web' ] 
+then
+  if [ "$HLL_HOST" == '' ] 
+  then
+      exit 0
+  fi
+  alembic upgrade head
+  ./manage.py init_db
+  ./manage.py register_api
+  cd rconweb 
+  ./manage.py makemigrations --no-input
+  ./manage.py migrate --noinput
+  ./manage.py collectstatic --noinput
+  echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'admin') if not User.objects.filter(username='admin').first() else None" | python manage.py shell
+  export LOGGING_FILENAME=api_$SERVER_NUMBER.log
+  gunicorn --preload -w $NB_API_WORKERS -k gthread --threads $NB_API_THREADS -t 120 -b 0.0.0.0 rconweb.wsgi
+  cd ..
+  ./manage.py unregister_api
+else
+if [ "$1" == 'debug' ] 
+then
+  tail -f manage.py
+fi
+if [ "$HLL_HOST" == '' ] 
+then
+    exit 0
+fi
+  sleep 10
+  env >> /etc/environment
+  export LOGGING_FILENAME=supervisor_$SERVER_NUMBER.log
+  supervisord -c /config/supervisord_$SERVER_NUMBER.conf || supervisord -c /config/supervisord.conf
+fi
+
